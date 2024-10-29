121d1ea30034956701b04f68880230a3057f179b
Alex Curnow <alex@curnows.com>
2024-10-29 22:57:52 +0000

    2

--- assets/js/fun.js
+++ assets/js/fun.js
@@ -5,10 +5,10 @@ var typewriter = new Typewriter(app, {
 });
 
 typewriter.typeString('powerful web presence')
-    .pauseFor(2500)
+    .pauseFor(1500)
     .deleteAll()
     .typeString('dynamic design')
-    .pauseFor(2500)
+    .pauseFor(1500)
     .deleteAll()
     .typeString('great website')
     .pauseFor(2500)
